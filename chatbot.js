586b40d1eafcb975af6c483507c74d7ad6729a43
Alek Karp <alek@Aleks-MacBook-Pro.local>
2025-01-27 16:26:42 -0700

    Add property flex

--- chatbot.js
+++ chatbot.js
@@ -134,6 +134,7 @@ const addMessagesToConversationHistory = (history) => {
       position: fixed;
       bottom: 60px;
       right: 20px;
+      display: flex;
       flex-direction: column;
       font-family: 'Tinos', serif; /* Apply Tinos font */
       align-items: flex-end;
